7ce6e3dbb31402185cee9b90fa3f42ea9d680618
Carlos Ochoa <uligibson@hotmail.com>
2023-06-06 20:16:15 -0600

    code has been refactored

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from urllib.parse import urlparse
 import datetime
 
 
+load_dotenv()
+
+
 def print_ts(message=""):
+    # Prints a message with a timestamp
     if len(message) == 0:
         print()
         return
@@ -18,51 +22,46 @@ def print_ts(message=""):
     print(f"{timestamp}: {message}")
 
 
+def validate_environment_variable(variable_name):
+    # Validates if an environment variable is set
+    variable = os.getenv(variable_name)
+    if variable is None:
+        raise EnvironmentError(f"Couldn't load the env variable '{variable_name}', check the .env file")
+    return variable
+
+
 def get_web_info(url):
-    # Function to scrape web page info
-    response = requests.get(url)
+    # Scrapes a webpage and returns its title and description
+
+    user_agent = os.getenv("USER_AGENT")
+    if user_agent is None:
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+
+    headers = {"User-Agent": user_agent}
+    response = requests.get(url, headers=headers)
+    
     soup = BeautifulSoup(response.text, "html.parser")
 
-    if len(soup.title.string) > 0:
-        title = soup.title.string
-    else:
-        title = ""
-
-    description = soup.find("meta", attrs={"name": "description"})
-
-    if description:
-        description = description["content"]
-    else:
-        description = "No description found."
+    title = soup.title.string if soup.title else ""
+    meta_description = soup.find("meta", attrs={"name": "description"})
+    description = meta_description["content"] if meta_description else ""
 
     return title, description
 
 
 def process_info_with_ai(url, title, description):
+    # Processes webpage info with an AI and returns a response
 
     language = os.getenv("LANGUAGE")
-
     if language is None:
         language = "english"
 
-    openai_api = os.getenv("OPENAI_API_KEY")
-    if openai_api is None:
-        print_ts("Please set OPENAI_API_KEY in .env file")
-        return "error"
-
-    openai.api_key = openai_api
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
     parsed_url = urlparse(url)
     hostname = parsed_url.netloc
 
-    # prompt, english only
-    # prompt = f"As a Notion page admin, you're tasked with creating Notion pages using the information provided as well as any pre-existing knowledge you have. The website has the title '{title}' and hostname '{hostname}', and its meta tag description is '{description}'. Please evaluate the title and, if necessary, simplify it for use in a Notion page (e.g., a title like 'Welcome to Flask etc...' could be simplified to 'Flask'). If there's no title, please replace it with 'No title found'. Following that, generate a detailed and engaging description. Remember not to include the URL or hostname in the description, and aim for a length between 400 to 550 characters. If there's insufficient information to create a comprehensive description, please use 'No information found'. Please format your final output like this: 'title|||description', which will make parsing easier."
-
-    # new prompt, multilanguage v1
-    # prompt = f"As a Notion page admin, you're tasked with creating Notion pages using the information provided as well as any pre-existing knowledge you have. The website has the title '{title}' and hostname '{hostname}', and its meta tag description is '{description}'. Please evaluate the title and, if necessary, simplify it for use in a Notion page (e.g., a title like 'Welcome to Flask etc...' could be simplified to 'Flask'). If there's no title, please replace it with 'No title found'. Following that, generate a detailed and engaging description. Remember not to include the URL or hostname in the description, and aim for a length between 400 to 550 characters. If there's insufficient information to create a comprehensive description, please use 'No information found'. Please format your final output like this: 'title|||description', which will make parsing easier. Lastly, please ensure that the title and description are in {language}."
-
-    # new prompt, multilanguage v2
-    prompt = f"As a Notion page admin, you're tasked with creating Notion pages using the information provided as well as any pre-existing knowledge you have. The website has the title '{title}' and hostname '{hostname}', and its meta tag description is '{description}'. Please evaluate the title and, if necessary, simplify it for use in a Notion page (e.g., a title like 'Welcome to Flask etc...' could be simplified to 'Flask'). If there's no title, please replace it with 'No title found'. Following that, generate a detailed and engaging description. Remember not to include the URL or hostname in the description, and aim for a length between 400 to 550 characters. If there's insufficient information to create a comprehensive description, please use 'No information found'. Be mindful to keep brand names and specific terminologies in their original language. Please format your final output like this: 'title|||description', which will make parsing easier. Lastly, please ensure that the title and description are in {language}."
+    prompt = f"As a Notion page admin, you're tasked with creating Notion pages using the information provided as well as any pre-existing knowledge you have. The website has the title '{title}' and hostname '{hostname}', and its meta tag description is '{description}'. Please evaluate the title and, if necessary, simplify it for use in a Notion page (e.g. if original title is like 'Welcome to Flask etc...' you could simplify it to just 'Flask'). If there's no title, please replace it with 'No title found'. Following that, generate a detailed and engaging description. Remember not to include the URL or hostname in the description, and aim for a length between 400 to 550 characters. If there's insufficient information to create a comprehensive description, please use 'No information found'. Be mindful to keep brand names and specific terminologies in their original language. Please format your final output like this: 'title|||description', which will make parsing easier. Lastly, please ensure that the title and description are in {language}."
 
     print_ts(f"Generating best page title and description with AI...")
 
@@ -78,7 +77,7 @@ def process_info_with_ai(url, title, description):
             top_p=1,
             frequency_penalty=0.5,
             presence_penalty=0.5,
-            stream=False
+            stream=False,
         )
 
         # End the timer
@@ -90,85 +89,115 @@ def process_info_with_ai(url, title, description):
         if response.choices and response.choices[0].text.strip():
             return response.choices[0].text.strip()
         else:
-            print_ts("OpenAI API unknow error")
-            return "error"
+            raise Exception("OpenAI API unknow error")
 
     except openai.OpenAIError as e:
-        print_ts(f"OpenAI API Error: {str(e)}")
-        return "error"
+        raise Exception(f"OpenAI API Error: {str(e)}")
 
     except Exception as e:
-        print_ts(f"Unexpected Error: {str(e)}")
-        return "error"
+        raise Exception(f"Unexpected Error: {str(e)}")
 
 
 def create_notion_page(url, title, description_content):
-    # Function to create a new page in Notion
-
+    # Creates a new page in Notion
     print_ts("Creating Notion page...")
 
     notion_api_key = os.getenv("NOTION_API_KEY")
-    if notion_api_key is None:
-        print_ts("Please set NOTION_API_KEY in .env file")
-        return "error"
-
     notion = Client(auth=notion_api_key)
 
     notion_db_id = os.getenv("NOTION_DB_ID")
-    if notion_db_id is None:
-        print_ts("Please set NOTION_DB_ID in .env file")
-        return "error"
 
     page = notion.pages.create(
         parent={"database_id": notion_db_id},
         properties={
-            "Nombre": {"title": [{"text": {"content": title}}]},
+            "Name": {"title": [{"text": {"content": title}}]},
             "URL": {"url": url},
         },
         # Page content
         children=[
             {
                 "object": "block",
-                "paragraph": {"rich_text": [{"text": {"content": description_content}}]},
+                "paragraph": {
+                    "rich_text": [{"text": {"content": description_content}}]
+                },
             }
-        ]
+        ],
     )
 
     return page
 
 
-def scrap_web_info(url):
-    print_ts(f"Scraping web info...")
-    title, description = get_web_info(url)
-    processed_info = process_info_with_ai(url, title, description)
-    processed_title, processed_description = processed_info.split("|||")
-    create_notion_page(url, processed_title, processed_description)
-
-
 def main():
+    # Main function
     print_ts()
     print_ts("Starting the script...")
     print_ts()
-    load_dotenv()
-    your_favorites_file = os.getenv("YOUR_FAVORITES_FILE")
-    if your_favorites_file is None:
-        print_ts("Please set YOUR_FAVORITES_FILE in .env file")
+    print_ts("Validating indispensable environment variables...")
+    try:
+        bookmarks_file = validate_environment_variable("BOOKMARKS_FILE")
+        print_ts(f"BOOKMARKS_FILE: ✓")
+        validate_environment_variable("OPENAI_API_KEY")
+        print_ts(f"OPENAI_API_KEY: ✓")
+        validate_environment_variable("NOTION_API_KEY")
+        print_ts(f"NOTION_API_KEY: ✓")
+        validate_environment_variable("NOTION_DB_ID")
+        print_ts(f"NOTION_DB_ID: ✓")
+    except EnvironmentError as e:
+        print_ts(f"{str(e)}")
         sys.exit()
 
-    with open(os.getenv("YOUR_FAVORITES_FILE"), "r") as f:
-        urls = f.readlines()
+    print_ts("Indispensable environment variables loaded successfully!")
+    print_ts()
+
+    try:
+        print_ts("Trying to load your bookmarks file...")
+        with open(bookmarks_file, "r") as f:
+            urls = f.readlines()
+            if len(urls) == 0:
+                raise Exception("Bookmarks file is empty")
+            print_ts("Bookmarks file loaded successfully!")
+            print_ts()
+    except FileNotFoundError:
+        print_ts(f"Failed to load your bookmarks file '{bookmarks_file}'")
+        sys.exit()
+
+    print_ts("Starting the main process...")
+    print_ts()
+
+    failed = 0
+    succeeded = 0
 
     for url in urls:
         # Remove newline characters
         url = url.strip()
         try:
             print_ts(f"[ ======= {url} ======= ]")
-            scrap_web_info(url)
+            #
+            # Scrapes a webpage, processes the info with an AI and creates a new page in Notion
+            print_ts(f"Scraping web info...")
+            title, description = get_web_info(url)
+            if len(title) == 0 and len(description) == 0:
+                raise Exception("Web scraping error")
+            if "cloudflare" in title.lower() or "cloudflare" in description.lower():
+                raise Exception("Web scraping error")
+
+            processed_info = process_info_with_ai(url, title, description)
+            processed_title, processed_description = processed_info.split("|||")
+            create_notion_page(url, processed_title, processed_description)
             print_ts("Successfully created Notion page")
-            print_ts()
+            succeeded += 1
         except Exception as e:
-            print_ts(f"Failed to create a page for {url}: {e}")
+            failed += 1
+            print_ts(f"Failed to create a Notion page. Error: {e}")
+        finally:
             print_ts()
+    
+    print_ts(f"Total URL's: {urls.__len__()}")
+    print_ts(f"Failed: {failed} | Succeeded: {succeeded}")
+
+    print_ts()
+    print_ts("Shutting down the script...")
+    sys.exit()
 
 
 # Run the main function
